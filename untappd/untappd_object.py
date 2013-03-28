f279d22072e853332c3fba60ad6e98fbeb4f35b8
Adam Ingrassia <adampsyche@gmail.com>
2013-03-28 18:49:57 -0400

    Add a few design comments.

--- untappd/untappd_object.py
+++ untappd/untappd_object.py
@@ -1,19 +1,20 @@
-
+#TODO do we want to set a base object for untappd objects? do they share enough
+# qualities to need a shared type?
 
 
 class _NotSetType:
-    def __repr__(self):
-        return "NotSet"  # pragma no cover
+  def __repr__(self):
+    return "NotSet"  # pragma no cover
 NotSet = _NotSetType()
 
 
 class BasicUntappdObject(object):
 
-    def __init__(self, attributes):
-        self._init_attributes()
-        self._setup_attributes(attributes)
+  def __init__(self, attributes):
+    self._init_attributes()
+    self._setup_attributes(attributes)
 
 
-
+#TODO determine what an Untappdobject vs a BasicUntappdObject would require
 class UntappdObject(BasicUntappdObject):
     pass
