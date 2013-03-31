b88ed7a136a5a049c0c941f98a4a9de93009cbba
Adam Ingrassia <adampsyche@gmail.com>
2013-03-31 00:28:23 -0400

    Adding some comments\\\

--- untappd/brewery.py
+++ untappd/brewery.py
@@ -5,7 +5,7 @@ import untappd_object
 class Brewery(untappd_object.UntappdObject):
 
   def __init__(self, brewery_id=None, brewery_active=None, brewery_name=None, 
-               contact=None, ):
+               contact=None, url=None, twitter=None, facebook=None):
                #TODO what do we do with nested stuff like this? Make them all 
                # attributes of the brewer, or of a contact subclass?
-, u'contact': {u'url': u'http://www.elysianbrewing.com/', u'twitter': u'elysianbrewing', u'facebook': u'http://www.facebook.com/pages/Elysian-Brewing-Company/92348396799'}, u'brewery_label': u'https://untappd.s3.amazonaws.com/site/brewery_logos/brewery-ElysianBrewingCompany_7294.jpeg', u'location': {u'brewery_city': u'Seattle', u'brewery_state': u'WA', u'lng': -122.316, u'lat': 47.614}, u'country_name': u'United States'}
+                u'elysianbrewing', u'facebook': u'http://www.facebook.com/pages/Elysian-Brewing-Company/92348396799'}, u'brewery_label': u'https://untappd.s3.amazonaws.com/site/brewery_logos/brewery-ElysianBrewingCompany_7294.jpeg', u'location': {u'brewery_city': u'Seattle', u'brewery_state': u'WA', u'lng': -122.316, u'lat': 47.614}, u'country_name': u'United States'}
