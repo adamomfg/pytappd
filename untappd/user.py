622e91c3ac4bbbba5f45d585fefc56323396e8f6
Adam Ingrassia <api@dhcp-172-26-77-251.nyc.corp.google.com>
2013-04-01 15:05:50 -0400

    meh, just making the calls make the calls and not create a user object.

--- untappd/user.py
+++ untappd/user.py
@@ -5,7 +5,3 @@ class User(object):
   def __init__(self, data):
   
     self.__dict__.update(data)
-    api = untappd.Untappd(client_id='C6D476FF7E034C2F10EB9A9F6975B93B1B82044B',
-                          client_secret='E23D250D26BD3237A75C05FF57D5DE93727D8A38')
-    self.badges = api.GetUserBadges(self.user_name)
-    self.distinct_beers = api.GetUserDistinctBeers(self.user_name)
