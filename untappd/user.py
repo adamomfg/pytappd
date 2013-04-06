d2a84b081011c7e8741510f619a6c4d17c024023
Adam Ingrassia <adampsyche@gmail.com>
2013-04-06 12:23:19 -0400

    initializing some attributes

--- untappd/user.py
+++ untappd/user.py
@@ -2,7 +2,12 @@ import untappd
 
 class User(object):
   
-  def __init__(self, api=None):
+  def __init__(self, api=None, bio=None, first_name=None, last_name=None,
+               user_avatar=None, uid=None, relationship=None, untappd_url=None,
+               url=None, checkins=None, settings=None, media=None, contact=None,
+               date_joined=None, location=None, account_type=None,
+               is_supporter=None, recent_brews=None, user_name=None, id=None,
+               is_private=None, stats=None):
   
     self.api = untappd.Api()
     
@@ -11,4 +16,6 @@ class User(object):
 
   def GetUserInfo(self, username):
     call = '/user/info/%s' % username
-    return untappd.Api.Call(self.api, call, self.api.key)
+    response_dict = untappd.Api.Call(self.api, call, self.api.key)
+    for k, v in response_dict['response']['user'].iteritems():
+      setattr(self, k, v)
