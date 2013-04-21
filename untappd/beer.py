4bea738929006b791033ceba69ab2048d0f1e715
Adam Ingrassia <adampsyche@gmail.com>
2013-04-21 14:57:11 -0400

    Make the beer search do something.

--- untappd/beer.py
+++ untappd/beer.py
@@ -2,7 +2,7 @@ import untappd
 
 class Beer(object):
 
-  def __init__(self, api, beer_id=None, name=None, label=None, abv=None, style=None,
+  def __init__(self, api=None, beer_id=None, name=None, label=None, abv=None, style=None,
                description=None, brewery=None):
 
     self.api = untappd.Api()
@@ -10,6 +10,12 @@ class Beer(object):
     if self.api is None:
       raise Exception('untappd.api is required.')
         
+  def SearchBeers(self, beer):
+    """Search for a beer."""
+    call = '/search/beer/?q=%s?sort=count' % beer
+    response_dict = untappd.Api.Call(self.api, call, self.api.payload)
+    return response_dict
+  
   def GetBeerInfo(self, beer):
     """Pass the beer id."""
     pass
