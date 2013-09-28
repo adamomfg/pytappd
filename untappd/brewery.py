f78f0610b267545baf290d528bdde7918e2854ab
Adam Ingrassia <adampsyche@gmail.com>
2013-09-28 12:48:32 -0400

    Initialize brewery object attributes.

--- untappd/brewery.py
+++ untappd/brewery.py
@@ -1,25 +1,29 @@
-__author__ = 'derek'
 import untappd_object
 
 
-class Brewery(untappd_object.UntappdObject):
+class Brewery(object):
 
   def __init__(self, brewery_id=None, brewery_active=None, brewery_name=None, 
                contact=None, url=None, twitter=None, facebook=None,
                brewery_label=None, location=None, brewery_city=None,
                brewery_state=None, lng=None, lat=None, country_name=None):
-               
-    self.brewery_id = brewery_id
+
     self.brewery_active = brewery_active
-    self.brewery_name = brewery_name
-    self.contact = contact
-    self.url = url
-    self.twitter = twitter
-    self.facebook = facebook
-    self.brewery_label = brewery_label
-    self.location = location
     self.brewery_city = brewery_city
+    self.brewery_id = brewery_id
+    self.brewery_label = brewery_label
+    self.brewery_name = brewery_name
     self.brewery_state = brewery_state
+    self.country_name = country_name    
+    self.contact = contact
+    self.facebook = facebook
+    self.lat = lat    
     self.lng = lng
-    self.lat = lat
-    self.country_name = country_name
+    self.location = location
+    self.twitter = twitter
+    self.url = url
+    
+    self.api = untappd.UntappdApi()
+
+    if self.api is None:
+      raise Exception('untappd.api is required.')
