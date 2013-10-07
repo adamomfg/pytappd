3a014a7c9e058b381b27c943b743305748ebf239
Chris Schrier <chris@redh4re.net>
2013-10-06 20:25:16 -0400

    Cleanup distinct_per_venue example

--- untappd/distinct_per_venue.py
+++ untappd/distinct_per_venue.py
@@ -1,33 +1,32 @@
 import user
 
+VENUE = 257163
+
+
+def GetUserCheckins(user_obj, limit=50, max_id=None):
+  request = {'limit': limit}
+  if max_id is not None:
+    request['max_id'] = max_id
+  return user_obj.GetUserCheckins(request)
+
+
+def IsPonyCheckin(checkin):
+  if 'venue' not in checkin:
+    return False
+  return checkin['venue'].get('venue_name') == 'The Pony Bar'
+
+
 lush = user.User(user_name='adampsyche')
-
 pony_checkins = []
-venue = 257163
 
-def GetPonyCheckins(checkin):
-  if checkin.get('venue'):
-    if checkin['venue']['venue_name'] == 'The Pony Bar':
-      return checkin
-  return None
+checkins = GetUserCheckins(lush)
+while True:
+  if not checkins:
+    break
 
-checkins = lush.GetUserCheckins({'limit': 50})
-for checkin in checkins:
-  if GetPonyCheckins(checkin):
-    pony_checkins.append(checkin)
-    
-marker_checkin = checkins[-1]['checkin_id']
+  pony_checkins.extend(filter(IsPonyCheckin, checkins))
+  marker_checkin = checkins[-1].get('checkin_id')
+  checkins = GetUserCheckins(lush, max_id=marker_checkin)
 
-while lush.GetUserCheckins({'limit': 50, 'max_id': marker_checkin}):
-  checkins = lush.GetUserCheckins({'limit': 50, 'max_id': marker_checkin})
-  if len(checkins) > 1:
-    for checkin in checkins:
-      if GetPonyCheckins(checkin):
-        pony_checkins.append(checkin)
-  marker_checkin = checkins[-1]['checkin_id']
-  
-pony_uniques = []
-
-for pony_checkin in pony_checkins:
-  if pony_checkin['beer']['beer_name'] not in pony_uniques:
-    pony_uniques.append(pony_checkin['beer']['beer_name'])
+pony_uniques = set()
+pony_uniques.update([c['beer']['beer_name'] for c in pony_checkins)
