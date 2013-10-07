56513ff3353d18b7ea7f2e51618fff9c2ef28203
Adam Ingrassia <adampsyche@gmail.com>
2013-10-07 09:24:42 -0400

    merging upstream

--- untappd/distinct_per_venue.py
+++ untappd/distinct_per_venue.py
@@ -18,38 +18,6 @@ def IsPonyCheckin(checkin):
 
 lush = user.User(user_name='adampsyche')
 pony_checkins = []
-<<<<<<< HEAD
-venue = 257163
-
-def GetPonyCheckins(checkin):
-  if checkin.get('venue'):
-    if checkin['venue']['venue_name'] == 'The Pony Bar':
-      return checkin
-  return None
-
-checkins = lush.GetUserCheckins({'limit': 50})
-for checkin in checkins:
-  if GetPonyCheckins(checkin):
-    pony_checkins.append(checkin)
-    continue
-
-marker_checkin = checkins[-1]['checkin_id']
-
-while lush.GetUserCheckins({'limit': 50, 'max_id': marker_checkin}):
-  checkins = lush.GetUserCheckins({'limit': 50, 'max_id': marker_checkin})
-  if len(checkins) > 1:
-    for checkin in checkins:
-      if GetPonyCheckins(checkin):
-        pony_checkins.append(checkin)
-  marker_checkin = checkins[-1]['checkin_id']
-  continue
-  
-pony_uniques = []
-
-for pony_checkin in pony_checkins:
-  if pony_checkin['beer']['beer_name'] not in pony_uniques:
-    pony_uniques.append(pony_checkin['beer']['beer_name'])
-=======
 
 checkins = GetUserCheckins(lush)
 while True:
@@ -62,4 +30,3 @@ while True:
 
 pony_uniques = set()
 pony_uniques.update([c['beer']['beer_name'] for c in pony_checkins])
->>>>>>> eef8331e57b88f16e647fa047a1dbe080614f148
